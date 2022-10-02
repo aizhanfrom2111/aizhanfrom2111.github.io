20481e519c507e2e100132a4b79d7aeab84ff40a
aizhanfrom2111 <114870778+aizhanfrom2111@users.noreply.github.com>
2022-10-02 21:53:25 +0600

    Delete Sunday.java

--- Sunday.java
+++
@@ -1,8 +0,0 @@
-package Week2.strategy1;
-
-public class Sunday implements WeGoJim {
-    @Override
-    public void toTrainWhat() {
-        System.out.println("Legs, Hamstring focused");
-    }
-}
