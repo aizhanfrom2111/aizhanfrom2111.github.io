196cbefdf350776e3dae11120d6a55094cbc0c3c
aizhanfrom2111 <114870778+aizhanfrom2111@users.noreply.github.com>
2022-10-02 21:52:57 +0600

    Delete Monday.java

--- Monday.java
+++
@@ -1,8 +0,0 @@
-package Week2.strategy1;
-
-public class Monday implements WeGoJim {
-    @Override
-    public void toTrainWhat() {
-    System.out.println("Chest, Triceps and Shoulders");
-    }
-}
